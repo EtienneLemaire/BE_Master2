721af89c7b65645e9a722238760573d14ac3bba9
EtienneLemaire <etienne.lemaire38@gmail.com>
2019-09-17 11:29:13 +0200

    Add files via upload

--- anenometre/anemometre/compteur.vhd
+++ anenometre/anemometre/compteur.vhd
@@ -3,45 +3,51 @@ use IEEE.std_logic_1164.all;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use ieee.numeric_std.all;
+use ieee.std_logic_arith;
 
 
-entity freq1Hz is
-    Port ( in_freq_anemometre,sig_1Hz : in  STD_LOGIC;    -- Horloge 50 Mhz et Reset Asynchrone
+entity compteur is
+    Port ( in_freq_anemometre,sig_1Hz, reset : in  STD_LOGIC;    -- Horloge 50 Mhz et Reset Asynchrone
            freq_signal : out  std_logic_vector (7 downto 0));         -- Horloge 1 MHz
-end freq1Hz;
+end compteur;
  
-architecture Behavioral of freq1Hz is
+architecture Behavioral of compteur is
  
 -- Registre Horloge 50 MHz
 
-signal switch: STD_LOGIC;
-signal compteur : std_logic_vector (7 downto 0);
+signal compteur_8bits : std_logic_vector (7 downto 0);
+signal Hz_reset : STD_LOGIC;
 begin
  
 -- Affectation Port de Sortie
 
 --------------------------------------------
 -- DIVISION PAR 4 DE L'HORLOGE 100 MHZ
-process(CLOCK_50,reset)
+process(sig_1Hz)
+		begin
+		if (sig_1Hz='1') then
+			Hz_reset <= '1';
+		else 
+			Hz_reset <= '0';
+		end if;
+end process;
+
+process(in_freq_anemometre, reset)
  
     begin
     
-    
-     
         if (reset = '1') then
-            compteur <= "00000000";  
+            compteur_8bits <= "00000000";  
      
-        elsif rising_edge(sig_1Hz) then
-			freq_signal <= compteur; 
-			compteur <= "00000000"; 
-				
+        elsif (Hz_reset = '0') then
+			compteur_8bits <= "00000000"; 		
 		else
-         if rising_edge(in_freq_anemometre) then
-					compteur <= compteur + '1';
-		 end if;
-        end if;
- 
-end process;
+			if rising_edge(in_freq_anemometre) then
+					compteur_8bits <= compteur_8bits + '1';
+			end if;
+		end if;
 		
+end process;
+freq_signal <= compteur_8bits+compteur_8bits;	
 		
 end Behavioral;
